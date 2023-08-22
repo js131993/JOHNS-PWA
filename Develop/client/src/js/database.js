2d2e5d41c9384cfb3de21ef11ef53600fe79b934
js131993 <john.sanders4444@gmail.com>
2023-08-22 13:45:36 -0400

    Update database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,13 +15,21 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await openDB("jate", 1);
+  // create a new transaction with readwrite access
   const tx = db.transaction("jate", "readwrite");
+  // access the jate object store
   const store = tx.objectStore("jate");
+  // put the content to the store
   await store.put({ id: 1, content: content });
+  // wait for the transaction to complete
   await tx.done;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB("jate", 1);
+  const record = await db.get("jate", 1);
+  return record ? record.content : null;
+};
 
 initdb();
