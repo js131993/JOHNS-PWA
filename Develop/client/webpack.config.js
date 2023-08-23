1bc414936e3c7e270768ffd9cdfa87fdbfa1901f
js131993 <john.sanders4444@gmail.com>
2023-08-22 20:57:13 -0400

    Update webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -6,25 +6,77 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      // Generate an HTML file for your application with a <script> injected.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        chunks: ["main", "install"],
+      }),
+
+      // Generate the manifest for PWA.
+      new WebpackPwaManifest({
+        filename: "manifest.json", // specify the output directory here
+        publicPath: "/",
+        name: "My PWA Text Editor",
+        short_name: "TextEditorPWA",
+        description: "A simple PWA Text Editor",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials", // can be null, use-credentials or anonymous
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+
+      // Workbox configuration to generate the service worker.
+      new InjectManifest({
+        swSrc: "./src-sw.js", // your custom service worker file
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
 };
+
